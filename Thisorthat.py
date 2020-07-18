ac26b3c2890338f151bc489e0887eb75fb47da67
Jood A.F <54289869+jood-git@users.noreply.github.com>
2020-07-18 03:18:43 +0300

    This or that --- Anime Shows

---
+++ Thisorthat.py
@@ -0,0 +1,135 @@
+
+
+echo "# Minigames" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/jood-git/Minigames.git
+git push -u origin master
+
+
+
+
+
+myname = 'Jood' 
+print ('hello my name is ' +  myname )
+
+yourname = input('what is your name?')
+print ('hello'+ yourname)
+
+
+print ('Ive made a this or that-type anime game! Do you want to play?')
+confirm = input ('?')
+
+print( 'Okay! lets begin.')
+
+print(' We will begin with the anime shows section!')
+
+
+anime1 = input ('do you perfer Naruto or One Piece?')
+print ("youve answered " + anime1)
+
+
+anime2 = input ('do you perfer ' +anime1 + ' or My Hero Academia?')
+print ('youve answered ' + anime2)
+
+
+anime3 = input ('do you perfer ' +anime2 + ' or Demon Slayer??')
+print ('youve answered ' + anime3 )   
+
+anime4 = input ('do you perfer ' +anime3 + ' or Bleach?')
+print ('youve answered ' + anime4 )    
+
+
+anime5 = input ('do you perfer ' +anime4 + ' or Fairytale?')
+print ('youve answered ' + anime5 )    
+
+
+anime6 = input ('do you perfer ' +anime5 + ' or Jojos Bizzare Adventures?')
+print ('youve answered ' + anime6 )    
+
+
+anime7 = input ('do you perfer ' +anime6 + ' or Doctor Stone?')
+print ('youve answered ' + anime7 )    
+
+
+anime8 = input ('do you perfer ' +anime7 + ' or Dragonball?')
+print ('youve answered ' + anime8 )    
+
+
+anime9 = input ('do you perfer ' +anime8 + ' or Black Clover?')
+print ('youve answered ' + anime9 )    
+
+anime10 = input ('do you perfer ' +anime9 + ' or Code Geass?')
+print ('youve answered ' + anime10 )    
+
+anime11= input ('do you perfer ' +anime10 + ' or Attack on Titan?')
+print ('youve answered ' + anime11 )    
+
+anime12= input ('do you perfer ' +anime11 + ' or One Punch Man?')
+print ('youve answered ' + anime12 )    
+
+anime13= input ('do you perfer ' +anime12 + ' or Hunter x Hunter?')
+print ('youve answered ' + anime13 )    
+
+anime14= input ('do you perfer ' +anime13 + ' or Seven Deadly Sins?')
+print ('youve answered ' + anime14 )    
+
+anime15= input ('do you perfer ' +anime14 + ' or Angel Beats?')
+print ('youve answered ' + anime15 )    
+
+anime16= input ('do you perfer ' +anime15 + ' or Toradora?')
+print ('youve answered ' + anime16 )    
+
+anime17= input ('do you perfer ' +anime16 + ' or The Disastrous Life of Saiki K.?')
+print ('youve answered ' + anime17 )    
+
+anime18= input ('do you perfer ' +anime17 + ' or  Ouran Highschool Host Club?')
+print ('youve answered ' + anime18 )    
+
+anime19= input ('do you perfer ' +anime18 + ' or Sword Art Online?')
+print ('youve answered ' + anime19 )    
+
+anime20= input ('do you perfer ' +anime19 + ' or Charlotte?')
+print ('youve answered ' + anime20 )  
+
+anime21= input ('do you perfer ' +anime20 + ' or The Promised Neverland?')
+print ('youve answered ' + anime21 )  
+
+anime22= input ('do you perfer ' +anime21 + ' or  Full Metal Alchemist?')
+print ('youve answered ' + anime22 )  
+
+anime23= input ('do you perfer ' +anime22 + ' or Your Lie in April?')
+print ('youve answered ' + anime23 )  
+
+anime24= input ('do you perfer ' +anime23 + ' or  Railgun?')
+print ('youve answered ' + anime24 ) 
+
+anime25= input ('do you perfer ' +anime24 + ' or  Fire Force?')
+print ('youve answered ' + anime25 )  
+
+anime26= input ('do you perfer ' +anime25 + ' or Cowboy Bebob?')
+print ('youve answered ' + anime26 )  
+
+anime27= input ('do you perfer ' +anime26 + ' or Akame ga Kill?')
+print ('youve answered ' + anime27 )  
+
+anime28= input ('do you perfer ' +anime27 + ' or Blue Exorist?')
+print ('youve answered ' + anime28 )  
+
+anime29= input ('do you perfer ' +anime28 + ' or HAIKYUU!?')
+print ('youve answered ' + anime29 )  
+
+anime30= input ('do you perfer ' +anime29 + ' or Mob Psycho 100?')
+print ('youve answered ' + anime30 + ',' + anime30 + ' wins!!!')   
+
+print(' CONGRATSSS <3 <3 <3 ')
+
+
+
+
+
+
+
+
+
